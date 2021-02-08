7f57e132a25c87cf7e89337d9e1277a554656ecc
umut-altay <66955138+umut-altay@users.noreply.github.com>
2021-02-08 09:12:37 +0100

    changes in code

--- resultProcessing.R
+++ resultProcessing.R
@@ -353,16 +353,16 @@ sampledLoc_jittered9=data.frame(Longitude = thetaSample_jittered[["xCoor_new"]][
 sampledLoc_jittered10=data.frame(Longitude = thetaSample_jittered[["xCoor_new"]][,10], Latitude = thetaSample_jittered[["yCoor_new"]][,10])
 
 # #Extract the corresponding observed locations
-# observed1=data.frame(Longitude = myData[["obs"]][["xCor"]][1], Latitude = myData[["obs"]][["yCor"]][1])
-# observed2=data.frame(Longitude = myData[["obs"]][["xCor"]][2], Latitude = myData[["obs"]][["yCor"]][2])
-# observed3=data.frame(Longitude = myData[["obs"]][["xCor"]][3], Latitude = myData[["obs"]][["yCor"]][3])
-# observed4=data.frame(Longitude = myData[["obs"]][["xCor"]][4], Latitude = myData[["obs"]][["yCor"]][4])
-# observed5=data.frame(Longitude = myData[["obs"]][["xCor"]][5], Latitude = myData[["obs"]][["yCor"]][5])
-# observed6=data.frame(Longitude = myData[["obs"]][["xCor"]][6], Latitude = myData[["obs"]][["yCor"]][6])
-# observed7=data.frame(Longitude = myData[["obs"]][["xCor"]][7], Latitude = myData[["obs"]][["yCor"]][7])
-# observed8=data.frame(Longitude = myData[["obs"]][["xCor"]][8], Latitude = myData[["obs"]][["yCor"]][8])
-# observed9=data.frame(Longitude = myData[["obs"]][["xCor"]][9], Latitude = myData[["obs"]][["yCor"]][9])
-# observed10=data.frame(Longitude = myData[["obs"]][["xCor"]][10], Latitude = myData[["obs"]][["yCor"]][10])
+observed1=data.frame(Longitude = myData[["obs"]][["xCor"]][1], Latitude = myData[["obs"]][["yCor"]][1])
+observed2=data.frame(Longitude = myData[["obs"]][["xCor"]][2], Latitude = myData[["obs"]][["yCor"]][2])
+observed3=data.frame(Longitude = myData[["obs"]][["xCor"]][3], Latitude = myData[["obs"]][["yCor"]][3])
+observed4=data.frame(Longitude = myData[["obs"]][["xCor"]][4], Latitude = myData[["obs"]][["yCor"]][4])
+observed5=data.frame(Longitude = myData[["obs"]][["xCor"]][5], Latitude = myData[["obs"]][["yCor"]][5])
+observed6=data.frame(Longitude = myData[["obs"]][["xCor"]][6], Latitude = myData[["obs"]][["yCor"]][6])
+observed7=data.frame(Longitude = myData[["obs"]][["xCor"]][7], Latitude = myData[["obs"]][["yCor"]][7])
+observed8=data.frame(Longitude = myData[["obs"]][["xCor"]][8], Latitude = myData[["obs"]][["yCor"]][8])
+observed9=data.frame(Longitude = myData[["obs"]][["xCor"]][9], Latitude = myData[["obs"]][["yCor"]][9])
+observed10=data.frame(Longitude = myData[["obs"]][["xCor"]][10], Latitude = myData[["obs"]][["yCor"]][10])
 
 #GGplot Figures
 
@@ -374,7 +374,10 @@ plot_data <-data.frame(X, Y, Label)
 ggplot(plot_data, aes(X, Y, group = Label)) +
   stat_density_2d(geom = "polygon",
                   aes(alpha = ..level.., fill = Label),
-                  bins = 4)  
+                  bins = 20) +
+  geom_point(aes(37.58623, 0.33945), color = "blue", size = 5) +
+  coord_fixed(ratio = 1)
+
 
 #Location2
 X=c(sampledLoc_original2[,1], sampledLoc_jittered2[,1])
@@ -382,10 +385,11 @@ Y=c(sampledLoc_original2[,2], sampledLoc_jittered2[,2])
 Label = c(rep('Original', 5000), rep('Jittered', 5000))
 plot_data <-data.frame(X, Y, Label)
 ggplot(plot_data, aes(X, Y, group = Label)) +
-  geom_point(data = observed2, aes(x = Longitude, y = Latitude), size = 1) +
   stat_density_2d(geom = "polygon",
                   aes(alpha = ..level.., fill = Label),
-                  bins = 4)  
+                  bins = 20) +
+  geom_point(aes(34.75349, 0.6045782), color = "blue", size = 5) +
+  coord_fixed(ratio = 1)
 
 #Location3
 X=c(sampledLoc_original3[,1], sampledLoc_jittered3[,1])
@@ -395,7 +399,9 @@ plot_data <-data.frame(X, Y, Label)
 ggplot(plot_data, aes(X, Y, group = Label)) +
   stat_density_2d(geom = "polygon",
                   aes(alpha = ..level.., fill = Label),
-                  bins = 4)  
+                  bins = 20) +
+  geom_point(aes(39.37971, -3.986475), color = "blue", size = 5) +
+  coord_fixed(ratio = 1) 
 
 #Location4
 X=c(sampledLoc_original4[,1], sampledLoc_jittered4[,1])
@@ -405,7 +411,10 @@ plot_data <-data.frame(X, Y, Label)
 ggplot(plot_data, aes(X, Y, group = Label)) +
   stat_density_2d(geom = "polygon",
                   aes(alpha = ..level.., fill = Label),
-                  bins = 4)  
+                  bins = 20) +
+  geom_point(aes(39.94585, -3.532408), color = "blue", size = 5) +
+  coord_fixed(ratio = 1) 
+
 
 #Location5
 X=c(sampledLoc_original5[,1], sampledLoc_jittered5[,1])
@@ -415,7 +424,9 @@ plot_data <-data.frame(X, Y, Label)
 ggplot(plot_data, aes(X, Y, group = Label)) +
   stat_density_2d(geom = "polygon",
                   aes(alpha = ..level.., fill = Label),
-                  bins = 4)  
+                  bins = 20) +
+  geom_point(aes(39.21714, -4.5904), color = "blue", size = 5) +
+  coord_fixed(ratio = 1)   
 
 #Location6
 X=c(sampledLoc_original6[,1], sampledLoc_jittered6[,1])
@@ -425,7 +436,9 @@ plot_data <-data.frame(X, Y, Label)
 ggplot(plot_data, aes(X, Y, group = Label)) +
   stat_density_2d(geom = "polygon",
                   aes(alpha = ..level.., fill = Label),
-                  bins = 4)  
+                  bins = 20) +
+  geom_point(aes(39.64854, -3.829867), color = "blue", size = 5) +
+  coord_fixed(ratio = 1)   
 
 #Location7
 X=c(sampledLoc_original7[,1], sampledLoc_jittered7[,1])
@@ -435,7 +448,10 @@ plot_data <-data.frame(X, Y, Label)
 ggplot(plot_data, aes(X, Y, group = Label)) +
   stat_density_2d(geom = "polygon",
                   aes(alpha = ..level.., fill = Label),
-                  bins = 4)  
+                  bins = 20) +
+  geom_point(aes(35.78805, -0.06940715), color = "blue", size = 5) +
+  coord_fixed(ratio = 1)   
+
 #Location8
 X=c(sampledLoc_original8[,1], sampledLoc_jittered8[,1])
 Y=c(sampledLoc_original8[,2], sampledLoc_jittered8[,2])
@@ -444,7 +460,9 @@ plot_data <-data.frame(X, Y, Label)
 ggplot(plot_data, aes(X, Y, group = Label)) +
   stat_density_2d(geom = "polygon",
                   aes(alpha = ..level.., fill = Label),
-                  bins = 4)  
+                  bins = 20) +
+  geom_point(aes(39.64046, -0.450423), color = "blue", size = 5) +
+  coord_fixed(ratio = 1) 
 
 #Location9
 X=c(sampledLoc_original9[,1], sampledLoc_jittered9[,1])
@@ -454,7 +472,9 @@ plot_data <-data.frame(X, Y, Label)
 ggplot(plot_data, aes(X, Y, group = Label)) +
   stat_density_2d(geom = "polygon",
                   aes(alpha = ..level.., fill = Label),
-                  bins = 4)  
+                  bins = 20) +
+  geom_point(aes(39.68441, -3.737138), color = "blue", size = 5) +
+  coord_fixed(ratio = 1) 
 
 #Location10
 X=c(sampledLoc_original10[,1], sampledLoc_jittered10[,1])
@@ -464,17 +484,11 @@ plot_data <-data.frame(X, Y, Label)
 ggplot(plot_data, aes(X, Y, group = Label)) +
   stat_density_2d(geom = "polygon",
                   aes(alpha = ..level.., fill = Label),
-                  bins = 4)  
-
+                  bins = 20) +
+  geom_point(aes(34.99275, -0.2633486), color = "blue", size = 5) +
+  coord_fixed(ratio = 1) 
 # #
-# #Location2
-# ggplot() +
-#    geom_point(data = sampledLoc_original2, aes(x = Longitude, y = Latitude, color = "Original"), size = 1) +
-#    geom_point(data = sampledLoc_jittered2, aes(x = Longitude, y = Latitude, color = "Jittered"), size = 1) +
-#    geom_point(data = observed2, aes(x = Longitude, y = Latitude, color = "Observed"), size = 1) +
-#    xlab('Longitude') +
-#    ylab('Latitude')+
-#    scale_colour_manual(values=c("orange","blue","yellow"))
+# #
 
 
 #Dawid Sebastiani Scores and RMSE for Old STAN Script based on Original Coordinates
@@ -508,7 +522,7 @@ DS_inlaOrig=((myData[["pred"]][["u"]]-mean_pred)/sd_pred)^2+log(sd_pred^2)
 
 sq_difference=list()
 sq_difference=(mean_pred-myData[["pred"]][["u"]])^2
-rmse_inla_original=(sum(unlist(sq_difference))/2500)^0.5
+rmse_inla_original=(sum(unlist(sq_difference))/1433)^0.5
 
 sDev=apply(uSample, 2, sd)
 DS_stanOldOrig=list()
@@ -516,18 +530,18 @@ DS_stanOldOrig=((myData[["pred"]][["u"]]-colMeans(uSample))/sDev)^2+log(sDev^2)
 
 sq_difference=list()
 sq_difference=(colMeans(uSample)-myData[["pred"]][["u"]])^2 
-rmse_stanold_original=(sum(unlist(sq_difference))/2500)^0.5
+rmse_stanold_original=(sum(unlist(sq_difference))/1433)^0.5
 
 #Nearest neighbour results and DS scores becomes a single data frame
 predPoint.index=c(1:length(loc.pred[,1]))
 distClass_oldOriginal=data.frame(pred.point = predPoint.index, nnDistance = nearestOriginal[,2], DS_inla = DS_inlaOrig, DS_stan = DS_stanOldOrig)
 
 boxplot(unlist(DS_stanOldOrig), horizontal=FALSE, ylim = c((min(c(unlist(DS_stanOldOrig),unlist(DS_inlaOrig)))-0.000000002), (max(c(unlist(DS_stanOldOrig), unlist(DS_inlaOrig)))+0.000000002)))
-title(sub ="DS Scores STAN OLD", line = 0)
+title(sub ="DS Scores STAN old-original", line = 0)
 abline(h=mean(unlist(DS_stanOldOrig)), col ="red")   #its own mean      
 abline(h=mean(unlist(DS_inlaOrig)), col ="blue") #mean crps obtained from INLA
 
-save(rmse_stanold_original, distClass_oldOriginal, file="results1.RData")
+save(rmse_stanold_original, distClass_oldOriginal, file="resultsOldOriginal.RData")
 
 #Dawid Sebastiani Scores and RMSE for New STAN Script based on Original Coordinates
 rm(list = ls())
@@ -561,7 +575,7 @@ DS_inlaOrig=((myData[["pred"]][["u"]]-mean_pred)/sd_pred)^2+log(sd_pred^2)
 
 sq_difference=list()
 sq_difference=(mean_pred-myData[["pred"]][["u"]])^2
-rmse_inla_original=(sum(unlist(sq_difference))/2500)^0.5
+rmse_inla_original=(sum(unlist(sq_difference))/1433)^0.5
 
 sDev=apply(uSample, 2, sd)
 DS_stanNewOrig=list()
@@ -569,18 +583,18 @@ DS_stanNewOrig=((myData[["pred"]][["u"]]-colMeans(uSample))/sDev)^2+log(sDev^2)
 
 sq_difference=list()
 sq_difference=(colMeans(uSample)-myData[["pred"]][["u"]])^2 
-rmse_stannew_original=(sum(unlist(sq_difference))/2500)^0.5
+rmse_stannew_original=(sum(unlist(sq_difference))/1433)^0.5
   
 #Nearest neighbour results and DS scores becomes a single data frame
 predPoint.index=c(1:length(loc.pred[,1]))
 distClass_newOriginal=data.frame(pred.point = predPoint.index, nnDistance = nearestOriginal[,2], DS_inla = DS_inlaOrig, DS_stan = DS_stanNewOrig)
 
 boxplot(unlist(DS_stanNewOrig),horizontal=FALSE, ylim = c((min(c(unlist(DS_stanNewOrig), DS_inlaOrig))-0.000000002), (max(c(unlist(DS_stanNewOrig), DS_inlaOrig))+0.000000002)))
-title(sub ="DS Scores STAN NEW", line = 0)
+title(sub ="DS Scores STAN new-original", line = 0)
 abline(h=mean(DS_stanNewOrig), col ="red")   #its own mean      
 abline(h=mean(DS_inlaOrig), col ="blue") #mean crps obtained from INLA
   
-save(rmse_inla_original, rmse_stannew_original, distClass_newOriginal, file="results2.RData")
+save(rmse_inla_original, rmse_stannew_original, distClass_newOriginal, file="resultsNewOriginal.RData")
 
 #Dawid Sebastiani Scores and RMSE for Old STAN Script based on Jittered Coordinates
 rm(list = ls())
@@ -614,7 +628,7 @@ DS_inlaJitt=((myData[["pred"]][["u"]]-mean_pred)/sd_pred)^2+log(sd_pred^2)
 
 sq_difference=list()
 sq_difference=(mean_pred-myData[["pred"]][["u"]])^2
-rmse_inla_jittered=(sum(unlist(sq_difference))/2500)^0.5
+rmse_inla_jittered=(sum(unlist(sq_difference))/1433)^0.5
 
 sDev=apply(uSample, 2, sd)
 DS_stanOldJitt=list()
@@ -622,18 +636,18 @@ DS_stanOldJitt=((myData[["pred"]][["u"]]-colMeans(uSample))/sDev)^2+log(sDev^2)
 
 sq_difference=list()
 sq_difference=(colMeans(uSample)-myData[["pred"]][["u"]])^2 
-rmse_stanold_jittered=(sum(unlist(sq_difference))/2500)^0.5
+rmse_stanold_jittered=(sum(unlist(sq_difference))/1433)^0.5
 
 #Nearest neighbour results and DS scores becomes a single data frame
 predPoint.index=c(1:length(loc.pred[,1]))
 distClass_oldJittered=data.frame(pred.point = predPoint.index, nnDistance = nearestJittered[,2], DS_inla = DS_inlaJitt, DS_stan = DS_stanOldJitt)
 
 boxplot(unlist(DS_stanOldJitt),horizontal=FALSE, ylim = c((min(c(unlist(DS_stanOldJitt), DS_inlaJitt))-0.000000002), (max(c(unlist(DS_stanOldJitt), DS_inlaJitt))+0.000000002)))
-title(sub ="DS Scores STAN OLD", line = 0)
+title(sub ="DS Scores STAN old-jittered", line = 0)
 abline(h=mean(DS_stanOldJitt), col ="red")   #its own mean      
 abline(h=mean(DS_inlaJitt), col ="blue") #mean crps obtained from INLA
 
-save(rmse_inla_jittered, rmse_stanold_jittered, distClass_oldJittered, file="results3.RData")
+save(rmse_inla_jittered, rmse_stanold_jittered, distClass_oldJittered, file="resultsOldJittered.RData")
 
 #Dawid Sebastiani Scores and RMSE for New STAN Script based on Jittered Coordinates
 rm(list = ls())
@@ -667,7 +681,7 @@ DS_inlaJitt=((myData[["pred"]][["u"]]-mean_pred)/sd_pred)^2+log(sd_pred^2)
 
 sq_difference=list()
 sq_difference=(mean_pred-myData[["pred"]][["u"]])^2
-rmse_inla_jittered=(sum(unlist(sq_difference))/2500)^0.5
+rmse_inla_jittered=(sum(unlist(sq_difference))/1433)^0.5
 
 sDev=apply(uSample, 2, sd)
 DS_stanNewJitt=list()
@@ -675,32 +689,41 @@ DS_stanNewJitt=((myData[["pred"]][["u"]]-colMeans(uSample))/sDev)^2+log(sDev^2)
 
 sq_difference=list()
 sq_difference=(colMeans(uSample)-myData[["pred"]][["u"]])^2 
-rmse_stannew_jittered=(sum(unlist(sq_difference))/2500)^0.5
+rmse_stannew_jittered=(sum(unlist(sq_difference))/1433)^0.5
 
 #Nearest neighbour results and DS scores becomes a single data frame
 predPoint.index=c(1:length(loc.pred[,1]))
 distClass_newJittered=data.frame(pred.point = predPoint.index, nnDistance = nearestJittered[,2], DS_inla = DS_inlaJitt, DS_stan = DS_stanNewJitt)
 
 boxplot(unlist(DS_stanNewJitt),horizontal=FALSE, ylim = c((min(c(unlist(DS_stanNewJitt), DS_inlaJitt))-0.000000002), (max(c(unlist(DS_stanNewJitt), DS_inlaJitt))+0.000000002)))
-title(sub ="DS Scores STAN NEW", line = 0)
+title(sub ="DS Scores STAN new-jittered", line = 0)
 abline(h=mean(DS_stanNewJitt), col ="red")   #its own mean      
 abline(h=mean(DS_inlaJitt), col ="blue") #mean crps obtained from INLA
 
-save(rmse_stannew_jittered, distClass_newJittered, file="results4.RData")
+save(rmse_stannew_jittered, distClass_newJittered, file="resultsNewJittered.RData")
 
 #Tabulation of RMSE values
 rm(list = ls())
 directory="~/Desktop/STAN sampling"
 setwd(directory)
 
-load("results1.RData")
-load("results2.RData")
-load("results3.RData")
-load("results4.RData")
+load("resultsOldOriginal.RData")
+load("resultsNewOriginal.RData")
+load("resultsOldJittered.RData")
+load("resultsNewJittered.RData")
 
 library(xtable)
 rmse=data.frame(Type=c("Original", "Jittered"), INLA=c(rmse_inla_original, rmse_inla_jittered), STAN1=c(rmse_stanold_original, rmse_stanold_jittered), STAN2=c(rmse_stannew_original, rmse_stannew_jittered))
 xtable(rmse)
+
+
+
+
+
+
+
+
+
 
 #Splitting the data into 10 classes with respect to the distances and calculating average DS scores for each class
 
@@ -709,10 +732,10 @@ rm(list = ls())
 directory="~/Desktop/STAN sampling"
 setwd(directory)
 
-load("results1.RData")
-load("results2.RData")
-load("results3.RData")
-load("results4.RData")
+load("resultsOldOriginal.RData")
+load("resultsNewOriginal.RData")
+load("resultsOldJittered.RData")
+load("resultsNewJittered.RData")
 
 #Define the class size
 #find a class size for 10 classes
@@ -720,6 +743,7 @@ size=(max(distClass_newOriginal$nnDistance)-min(distClass_newOriginal$nnDistance
 #breaks
 b=c(-Inf, 1:9, Inf)
 b=size*b
+km=b*111
 #labels
 l=c(1:10) # labels should be 1 less than the breaks
 
@@ -728,7 +752,7 @@ nnd=distClass_newOriginal$nnDistance
 cls=cut(nnd, breaks = b, labels = l)
 distClass_newOriginal$cls=cls
 avrScores_newOriginal=aggregate(distClass_newOriginal[, 3:4], list(distClass_newOriginal$cls), mean)
-
+avrScores_newOriginal=data.frame(avrScores_newOriginal,Lower = km[1:10], Upper = km[2:11])
 library(xtable)
 xtable(avrScores_newOriginal)
 
@@ -738,10 +762,10 @@ rm(list = ls())
 directory="~/Desktop/STAN sampling"
 setwd(directory)
 
-load("results1.RData")
-load("results2.RData")
-load("results3.RData")
-load("results4.RData")
+load("resultsOldOriginal.RData")
+load("resultsNewOriginal.RData")
+load("resultsOldJittered.RData")
+load("resultsNewJittered.RData")
 
 #Define the class size
 #find a class size for 10 classes
@@ -749,6 +773,7 @@ size=(max(distClass_oldOriginal$nnDistance)-min(distClass_oldOriginal$nnDistance
 #breaks
 b=c(-Inf, 1:9, Inf)
 b=size*b
+km=b*111
 #labels
 l=c(1:10) # labels should be 1 less than the breaks
 
@@ -757,6 +782,7 @@ nnd=distClass_oldOriginal$nnDistance
 cls=cut(nnd, breaks = b, labels = l)
 distClass_oldOriginal$cls=cls
 avrScores_oldOriginal=aggregate(distClass_oldOriginal[, 3:4], list(distClass_oldOriginal$cls), mean)
+avrScores_oldOriginal=data.frame(avrScores_oldOriginal,Lower = km[1:10], Upper = km[2:11])
 
 library(xtable)
 xtable(avrScores_oldOriginal)
@@ -767,10 +793,10 @@ rm(list = ls())
 directory="~/Desktop/STAN sampling"
 setwd(directory)
 
-load("results1.RData")
-load("results2.RData")
-load("results3.RData")
-load("results4.RData")
+load("resultsOldOriginal.RData")
+load("resultsNewOriginal.RData")
+load("resultsOldJittered.RData")
+load("resultsNewJittered.RData")
 
 #Define the class size
 #find a class size for 10 classes
@@ -778,6 +804,7 @@ size=(max(distClass_newJittered$nnDistance)-min(distClass_newJittered$nnDistance
 #breaks
 b=c(-Inf, 1:9, Inf)
 b=size*b
+km=b*111
 #labels
 l=c(1:10) # labels should be 1 less than the breaks
 
@@ -786,6 +813,7 @@ nnd=distClass_newJittered$nnDistance
 cls=cut(nnd, breaks = b, labels = l)
 distClass_newJittered$cls=cls
 avrScores_newJittered=aggregate(distClass_newJittered[, 3:4], list(distClass_newJittered$cls), mean)
+avrScores_newJittered=data.frame(avrScores_newJittered,Lower = km[1:10], Upper = km[2:11])
 
 library(xtable)
 xtable(avrScores_newJittered)
@@ -796,10 +824,10 @@ rm(list = ls())
 directory="~/Desktop/STAN sampling"
 setwd(directory)
 
-load("results1.RData")
-load("results2.RData")
-load("results3.RData")
-load("results4.RData")
+load("resultsOldOriginal.RData")
+load("resultsNewOriginal.RData")
+load("resultsOldJittered.RData")
+load("resultsNewJittered.RData")
 
 #Define the class size
 #find a class size for 10 classes
@@ -807,6 +835,7 @@ size=(max(distClass_oldJittered$nnDistance)-min(distClass_oldJittered$nnDistance
 #breaks
 b=c(-Inf, 1:9, Inf)
 b=size*b
+km=b*111
 #labels
 l=c(1:10) # labels should be 1 less than the breaks
 
@@ -815,6 +844,7 @@ nnd=distClass_oldJittered$nnDistance
 cls=cut(nnd, breaks = b, labels = l)
 distClass_oldJittered$cls=cls
 avrScores_oldJittered=aggregate(distClass_oldJittered[, 3:4], list(distClass_oldJittered$cls), mean)
+avrScores_oldJittered=data.frame(avrScores_oldJittered,Lower = km[1:10], Upper = km[2:11])
 
 library(xtable)
 xtable(avrScores_oldJittered)
